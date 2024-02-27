337e96c54f39acfe80a5efec52cf4ea45867f15a
Pierre Plourde <pierre@spartancsharp.net>
2024-02-27 11:18:26 -0500

    Create Code
    Build a Console Application that asks a user for their name and their age. If their name is Bob or Sue, address them as Professor. If the person is under 21, recommend they wait X years to start this class.

--- StudentCheckConsoleApp/Program.cs
+++ StudentCheckConsoleApp/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("What is your first name: ");
+string? firstName = Console.ReadLine();
+
+bool isValidAge;
+int age;
+
+do
+{
+	Console.Write("What is your age: ");
+	string? ageText = Console.ReadLine();
+
+	isValidAge = int.TryParse(ageText, out age);
+
+	if ( !isValidAge )
+	{
+		Console.WriteLine("That is not a valid age; please try again.");
+	}
+} while ( !isValidAge );
+
+Console.WriteLine();
+
+if ( firstName.ToLower() == "bob" || firstName.ToLower() == "sue" )
+{
+	Console.WriteLine("Welcome Professor!");
+}
+else if ( age >= 21 )
+{
+	Console.WriteLine($"Welcome {firstName}!");
+}
+else
+{
+	int yearsToWait = 21 - age;
+	Console.WriteLine($"You should wait {yearsToWait} years to start this class.");
+}
+
+Console.ReadLine();
